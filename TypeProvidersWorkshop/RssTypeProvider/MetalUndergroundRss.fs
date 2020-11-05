dd16e3b1af1ed739f881c4e59230486895a34206
Jorge Yañez <ofeth@hotmail.com>
2020-11-05 11:36:52 -0700

    Add RSS type providers project

---
+++ TypeProvidersWorkshop/RssTypeProvider/MetalUndergroundRss.fs
@@ -0,0 +1,35 @@
+﻿module MetalUndergroundRss
+
+open FSharp.Data
+
+let [<Literal>] private Url =
+    "http://feeds.feedburner.com/MetalUnderground"
+
+type private RSS = XmlProvider<Url>
+
+let private separator () =
+    printfn ""
+    [1 .. 100]
+    |> List.iter (fun _ -> printf "=")
+    printfn ""
+
+let getFeed () =
+    let feed = RSS.GetSample()
+    
+    let channel = feed.Channel
+    let title = channel.Title
+    let items = channel.Items
+
+    separator ()
+    printfn "SITE: %s" title
+    separator ()
+
+    items
+    |> List.ofArray
+    |> List.iter (fun item ->
+        printfn "TITLE: %s" item.Title
+        printfn "LINK %s" item.Link
+        separator ()
+    )
+
+    ()
---
+++ TypeProvidersWorkshop/RssTypeProvider/Program.fs
@@ -0,0 +1,9 @@
+﻿[<EntryPoint>]
+let main _ =
+    printfn "RSS TYPE PROVIDERS WITH F#!"
+
+    MetalInjectionRss.getFeed () |> ignore
+    MetalSucksRss.getFeed () |> ignore
+    MetalUndergroundRss.getFeed () |> ignore
+
+    0
