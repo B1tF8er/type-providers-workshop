ce1129fb071a6cc2c5344f847c7eda5b6816e73b
Jorge Yañez <ofeth@hotmail.com>
2020-11-05 16:11:45 -0700

    Add RSS handler module to ask and handle user input

--- TypeProvidersWorkshop/RssTypeProvider/Program.fs
+++ TypeProvidersWorkshop/RssTypeProvider/Program.fs
@@ -1,57 +1,5 @@
-﻿open Domain
-open System
-
-let private rssFeeds = [
-    MetalInjectionRss.create ()
-    MetalSucksRss.create ()
-    MetalUndergroundRss.create ()
-]
-
-let private get rssFeed = rssFeed.getFeed ()
-
-let private plot rssFeed = rssFeed.plotFeed ()
-
-let private save rssFeed = rssFeed.saveFeed ()
-
-let private readUserAction () =
-    Console.ReadKey().KeyChar.ToString().ToLowerInvariant()
-
-let [<Literal>] private AllowedActions =
-    "Allowed actions are: (g) Get Feeds | (p) Plot Feeds | (s) Save Feeds | (e) Exit"
-
-let private UserActionErrorMessage =
-    sprintf "Invalid user action, %s" AllowedActions
-
-let private printMessage message =
-    printfn ""
-    printfn "%s" message
-
-let askUserAction () =
-    AllowedActions |> printMessage
-
-    match readUserAction () with
-    | "g" ->
-        rssFeeds |> List.iter get
-        true
-    | "p" ->
-        rssFeeds |> List.iter plot
-        true
-    | "s" ->
-        rssFeeds |> List.iter save
-        true
-    | "e" ->
-        false
-    | _ ->
-        UserActionErrorMessage |> printMessage
-        true
-
-[<EntryPoint>]
+﻿[<EntryPoint>]
 let main _ =
     printfn "RSS TYPE PROVIDERS WITH F#!"
-
-    let mutable askAgain = true
-
-    while (askAgain) do
-        askAgain <- askUserAction ()
-
+    RssHandler.askUserInput ()
     0
---
+++ TypeProvidersWorkshop/RssTypeProvider/RssHandler.fs
@@ -0,0 +1,66 @@
+﻿module RssHandler
+
+open Domain
+open System
+
+let private rssFeeds = [
+    MetalInjectionRss.create ()
+    MetalSucksRss.create ()
+    MetalUndergroundRss.create ()
+]
+
+let private get rssFeed =
+    rssFeed.getFeed ()
+
+let private plot rssFeed =
+    rssFeed.plotFeed ()
+
+let private save rssFeed =
+    rssFeed.saveFeed ()
+
+let private readUserAction () =
+    Console.ReadKey().KeyChar
+
+let private printAllowedActions () =
+    printfn ""
+    printfn "Allowed actions are:"
+    printfn "(g) - Get Feeds"
+    printfn "(p) - Plot Feeds"
+    printfn "(s) - Save Feeds"
+    printfn "(e) - Exit"
+
+let private printUserActionErrorMessage () =
+    printfn ""
+    printfn ""
+    printfn "Invalid user action"
+
+let private printAction message =
+    printfn ""
+    printfn message
+
+let private ask () =
+    printAllowedActions ()
+
+    match readUserAction () with
+    | 'g' ->
+        printAction "Getting Feeds..."
+        rssFeeds |> List.iter get
+        true
+    | 'p' ->
+        printAction "Plotting Feeds..."
+        rssFeeds |> List.iter plot
+        true
+    | 's' ->
+        printAction "Savings Feeds..."
+        rssFeeds |> List.iter save
+        true
+    | 'e' ->
+        false
+    | _ ->
+        printUserActionErrorMessage ()
+        true
+
+let rec askUserInput () =
+    match ask () with
+    | true -> askUserInput ()
+    | false -> ()
