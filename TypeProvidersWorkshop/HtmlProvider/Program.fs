24e359c76f21e1c4e1fffac30bb3dbb3429fdc60
Jorge Yañez <ofeth@hotmail.com>
2020-11-18 00:51:21 -0700

    Add queen discography type provider

--- TypeProvidersWorkshop/HtmlProvider/Program.fs
+++ TypeProvidersWorkshop/HtmlProvider/Program.fs
@@ -2,4 +2,5 @@
 let main _ =
     printfn "HTML TYPE PROVIDERS WITH F#!"
     NugetProvider.run ()
+    QueenTypeProvider.run ()
     0
---
+++ TypeProvidersWorkshop/HtmlProvider/QueenTypeProvider.fs
@@ -0,0 +1,28 @@
+﻿module QueenTypeProvider
+
+open FSharp.Data
+
+[<Literal>]
+let QueenDiscographyUrl = "https://en.wikipedia.org/wiki/Queen_discography"
+
+type QueenDiscography = HtmlProvider<QueenDiscographyUrl>
+
+let getData () =
+    let sample = QueenDiscography.GetSample()
+
+    sample.Tables.``Studio albums``.Rows
+    |> Array.iter (fun album ->
+        printfn "%s - %A" album.Title album.``Album details``
+    )
+
+    [
+        sample.Tables.``1970s``.Rows,
+        sample.Tables.``1980s``.Rows,
+        sample.Tables.``1990s``.Rows,
+        sample.Tables.``2000s``.Rows,
+        sample.Tables.``2010s``.Rows
+    ]
+    |> List.iter (fun singles -> printfn "%A" singles)
+
+let run () =
+    getData ()
