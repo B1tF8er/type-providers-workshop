8750c419237355e9a32ddb61e9e2131e21072f42
Jorge Yañez <ofeth@hotmail.com>
2020-11-18 01:42:28 -0700

    Add functionality to plot Queen's discography

--- TypeProvidersWorkshop/HtmlProvider/QueenTypeProvider.fs
+++ TypeProvidersWorkshop/HtmlProvider/QueenTypeProvider.fs
@@ -1,6 +1,7 @@
 ﻿module QueenTypeProvider
 
 open FSharp.Data
+open XPlot.GoogleCharts
 
 [<Literal>]
 let QueenDiscographyUrl = "https://en.wikipedia.org/wiki/Queen_discography"
@@ -24,5 +25,33 @@ let getData () =
     ]
     |> List.iter (fun singles -> printfn "%A" singles)
 
+let asColumn (sample : QueenDiscography) (row : QueenDiscography.``1970s``.Row) =
+    let singlesPerYear =
+        sample.Tables.``1970s``.Rows
+        |> Array.filter (fun r -> r.Year = row.Year)
+        |> Array.length
+
+    (row.Year.ToString(), singlesPerYear)
+
+let getReleaseYears (sample : QueenDiscography) =
+    sample.Tables.``1970s``.Rows
+    |> List.ofSeq
+    |> List.map (fun row -> sprintf "Singles from %i" row.Year)
+    |> List.distinct
+
+let plotData () =
+    let sample = QueenDiscography.GetSample()
+
+    sample.Tables.``1970s``.Rows
+    |> Seq.ofArray
+    |> Seq.map (sample |> asColumn)
+    |> Seq.groupBy (fun column -> column |> fst)
+    |> Seq.map (fun group -> group |> snd)
+    |> Chart.Column
+    |> Chart.WithOptions (Options (title = "1970's Singles"))
+    |> Chart.WithLabels (sample |> getReleaseYears)
+    |> Chart.Show
+
 let run () =
     getData ()
+    plotData ()
