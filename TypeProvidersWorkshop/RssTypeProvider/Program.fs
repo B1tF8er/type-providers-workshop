d919d5d92570834aa50196eff119cda111f483b8
Jorge Yañez <ofeth@hotmail.com>
2020-11-17 21:44:46 -0700

    Remove base implementations of type providers

--- TypeProvidersWorkshop/RssTypeProvider/Program.fs
+++ TypeProvidersWorkshop/RssTypeProvider/Program.fs
@@ -1,9 +1,4 @@
 ﻿[<EntryPoint>]
 let main _ =
     printfn "RSS TYPE PROVIDERS WITH F#!"
-
-    MetalInjectionRss.getFeed () |> ignore
-    MetalSucksRss.getFeed () |> ignore
-    MetalUndergroundRss.getFeed () |> ignore
-
     0
